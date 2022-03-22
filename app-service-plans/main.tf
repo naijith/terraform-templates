1cdd0e91bd86cf599c0e7cbe76e3e708c05b6b6e
Naijith Gopal <gnaijith78@gmail.com>
2022-03-22 22:53:12 +0530

    app-service-plans

---
+++ app-service-plans/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_app_service_plan" "main" {
+  name                = "${var.prefix}-asp"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
